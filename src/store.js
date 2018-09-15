88b1e448072fc1b62495735bc0c32c87da9737d5
cheese <meng.qinglong@immomo.com>
2018-09-15 15:13:59 +0800

    1

--- src/store.js
+++ src/store.js
@@ -7,9 +7,20 @@ export default new Vuex.Store({
   state: {
     money: 20000
   },
+  getters: {
+    income_tax: state => {
+      return state.money * 0.1;
+    },
+    tax51: state => {
+      return state.money * 0.2;
+    },
+    pay: (state, getters) => {
+      return state.money - getters.tax51 - getters.income_tax;
+    }
+  },
   mutations: {
-    get_money(state, cash) {
-      state.money = state.money - cash;
+    set_money(state, new_money) {
+      state.money = state.money * 1 + new_money * 1;
     }
   }
 });
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,34 +1,46 @@
 <template>
   <div class="about">
-    <div>我银行里有￥{{money}}</div>
+    <div>月薪税前￥{{money}}</div>
+    <div>月薪税后￥{{pay}}</div>
+    <div style="color:#999">个税￥{{income_tax}}</div>
+    <div style="color:#999">五险一金￥{{tax51}}</div>
     <div>
-      取款机：
       <input type="text" v-model="v">
-      <button @click="get">取钱</button>
+      <button @click="get">升值加薪</button>
     </div>
-
-    <div>我钱包里有￥{{cash}}</div>
+    <!-- <div>我钱包里有￥{{cash}}</div> -->
     
   </div>
 </template>
 <script>
+import { mapState, mapGetters } from "vuex";
 export default {
   data() {
     return {
-      // money: this.$store.state.money,
       cash: 0,
       v: 0
     };
   },
   computed: {
-    money() {
-      return this.$store.state.money;
-    }
+    ...mapState(["money"]),
+    ...mapGetters(["income_tax", "tax51", "pay"])
+    // money() {
+    //   return this.$store.state.money;
+    // },
+    // income_tax() {
+    //   return this.$store.getters.income_tax;
+    // },
+    // tax51() {
+    //   return this.$store.getters.tax51;
+    // },
+    // pay() {
+    //   return this.$store.getters.pay;
+    // }
   },
   methods: {
     get() {
       this.cash += this.v * 1;
-      this.$store.commit("get_money", this.v);
+      this.$store.commit("set_money", this.v);
     }
   }
 };
